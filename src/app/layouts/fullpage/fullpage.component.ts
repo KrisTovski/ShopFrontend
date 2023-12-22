de89153fbaa4373dbfbe762c7aa9d237d4939cd6
krzysztof.filak <krzysztof.filak@siemens.com>
2023-12-22 14:26:04 +0100

    full page layout

---
+++ src/app/layouts/fullpage/fullpage.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fullpage',
+  templateUrl: './fullpage.component.html',
+  styleUrls: ['./fullpage.component.scss'],
+})
+export class FullpageComponent {}
